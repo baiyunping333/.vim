8f5f55dbcce9a31272be025d59564f6687106f8d
Konstantin Hristov <f.disclosure@gmail.com>
2012-11-15 10:36:32 +0100

    adding generate script for tags

---
+++ tags/generateTags.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# First make sure apt-file is install and then do:
+
+sudo apt-file update
+
+# set up tags for libc, the standard C library
+
+apt-file list libc6-dev | grep -o '/usr/include/.*\.h'> ~/.vim/tags/libc6-filelist
+ctags --sort=foldcase --c++-kinds=+p --fields=+iaS --extra=+q -I./libc6-ignore -f ~/.vim/tags/libc6 -L ~/.vim/tags/libc6-filelist 
+
+# create tags for stdlibc++ and STL
+
+apt-file list libstdc++6-4.4-dev | grep -E -o '/usr/include/.*\.(h|hpp)' > ~/.vim/tags/stdlibcpp-filelist
+ctags --sort=foldcase -R --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.vim/tags/stdlibcpp -L ~/.vim/tags/stdlibcpp-filelist
+
+# For Boost
+
+apt-file list boost | grep -E -o '/usr/include/.*\.(h|hpp)' | grep -v '/usr/include/boost/typeof/' > ~/.vim/tags/boost-filelist
+ctags --sort=foldcase --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.vim/tags/boost -L ~/.vim/tags/boost-filelist 
+
